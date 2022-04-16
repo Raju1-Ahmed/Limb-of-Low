fb05b6f3bb0f1930423c11b1dd0a4d594142afdf
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-04-17 03:16:49 +0600

    first commit header and footer add simple

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
